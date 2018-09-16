6b6e2f0efe1e21454081a5afefeb54b8f2f50a7f
jayxx1234 <jayxx1234@gmail.com>
2018-09-16 20:52:09 +1200

    Calculate fees in transactions, fix bugs, improve logging

--- index.mjs
+++ index.mjs
@@ -4,10 +4,14 @@ import MovingAverage from './strategies/moving-average';
 import config from './config/config';
 
 process.on('unhandledRejection', error => {
-	// Will print "unhandledRejection err is not defined"
 	console.log('unhandledRejection:', error);
 	process.exit();
 });
+
+// fee is a portion of the base (BTC) added to the cost of buy's, and removed from the return of sell's
+let fees = {
+	cryptopia: 0.002,
+};
 
 let exchange = Cryptopia();
 
@@ -16,25 +20,47 @@ const currentStrategy = MovingAverage;
 exchange.setOptions(config);
 
 let btcAmount = 0;
+let initialBtcAmount = 0;
+let coinAmount = 0;
+let coin = 'ETH';
 
 exchange.getBalance({
 	Currency: 'BTC'
 }).then(response => {
-	btcAmount = response.Data[0].Available;
+	initialBtcAmount = btcAmount = response.Data[0].Available;
+	logBalance();
 	main();
 });
 
+function logBalance() {
+	if (coinAmount == 0) {
+		let profit = Math.round((btcAmount - initialBtcAmount) * 100000000); // 10e8
+		console.log(`Current balance: ${btcAmount.toFixed(8)} BTC, ${coinAmount.toFixed(8)} ${coin}, profit: ${profit.toLocaleString('en')} Satoshi (roughly ${profit / 10000} NZD)`);
+	} else {
+		console.log(`Current balance: ${btcAmount.toFixed(8)} BTC, ${coinAmount.toFixed(8)} ${coin}`);
+	}
+}
+
 async function main() {
 	let period = 1000;
-	let pair = 'ETH_BTC';
+	let pair = `${coin}_BTC`;
 
 	// amount of ETH to buy/sell per trade
 	let tradeAmount = 0.001;
-	let currentTradeId = null;
 
 	let isUsingHistoricalData = true;
-	let historicalData = await exchange.getMarketHistory({Market: pair, Hours: 24 * 4});
+	let historicalDays = 100; // cryptopia caps at 1000 rows
+	let historicalData = await exchange.getMarketHistory({Market: pair, Hours: 24 * historicalDays});
 	let historicalRowNumber = 0;
+
+	// { id: 1, price: 3, amount: 0.001, feeRate: 0.002, total: (3 * 0.001) - 0.00006, type: 'buy' }
+	let pendingTrade = null;
+	let calcFee = function(trade) {
+		return trade.price * trade.amount * trade.feeRate;
+	}
+	let calcBase = function(trade) {
+		return trade.price * trade.amount - calcFee(trade);
+	}
 
 	let getValue = async function() {
 		if (isUsingHistoricalData)
@@ -54,57 +80,135 @@ async function main() {
 	}
 
 	let update = async function() {
+		if (isUsingHistoricalData && historicalRowNumber >= historicalData.Data.length) {
+			console.log(`End of historical data. Initial holdings: ${initialBtcAmount} BTC, 0 ${coin}`);
+			console.log(`Final holdings: ${btcAmount} BTC, ${coinAmount} ${coin}`);
+			console.log(`Difference: ${btcAmount - initialBtcAmount} BTC, ${coinAmount} ${coin}`);
+			return;
+		}
+
 		let currentValue = await getValue();
 		let action = trader.update(currentValue);
 
 		switch (action) {
-			case 'sell':
+			case 'sell': // sell all coin at this price
+				if (coinAmount <= 0) {
+					console.log('None left to sell');
+					break;
+				}
 				if (!isUsingHistoricalData) {
-					currentTradeId = exchange.submitTrade({
+					let currentTradeId = await exchange.submitTrade({
 						Market: pair,
 						Type: 'Sell',
 						Rate: currentValue,
-						Amount: tradeAmount,
+						Amount: coinAmount,
 					});
+					pendingTrade = {
+						id: currentTradeId,
+						price: currentValue,
+						amount: coinAmount,
+						feeRate: fees.cryptopia,
+						type: 'sell',
+					};
+				} else {
+					pendingTrade = {
+						id: -1,
+						price: currentValue,
+						amount: coinAmount,
+						feeRate: fees.cryptopia,
+						type: 'sell',
+					};
 				}
+				btcAmount += calcBase(pendingTrade);
+				coinAmount = 0;
 				console.log(`Sell at ${currentValue}`);
 				break;
-			case 'buy':
+			case 'buy': // buy tradeAmount coin at this price
+				if (btcAmount <= 0) {
+					console.log('No BTC left to buy with');
+					break;
+				}
 				if (!isUsingHistoricalData) {
-					currentTradeId = exchange.submitTrade({
+					let currentTradeId = exchange.submitTrade({
 						Market: pair,
 						Type: 'Buy',
 						Rate: currentValue,
 						Amount: tradeAmount,
 					});
+					pendingTrade = {
+						id: currentTradeId,
+						price: currentValue,
+						amount: tradeAmount,
+						feeRate: fees.cryptopia,
+						type: 'buy',
+					};
+				} else {
+					pendingTrade = {
+						id: -1,
+						price: currentValue,
+						amount: tradeAmount,
+						feeRate: fees.cryptopia,
+						type: 'buy',
+					};
 				}
+				coinAmount += pendingTrade.amount;
+				btcAmount -= calcBase(pendingTrade);
+
 				console.log(`Buy at ${currentValue}`);
 				break;
-			case 'exit sell':
+			case 'exit sell': // cancel the last sell order
+				if (pendingTrade == null) {
+					console.log('No sell trade to cancel');
+					break;
+				}
 				if (!isUsingHistoricalData) {
 					exchange.cancelTrade({
 						Type: 'Trade',
-						OrderId: currentTradeId,
+						OrderId: pendingTrade.id,
 					});
 				}
+				coinAmount += pendingTrade.amount;
+				btcAmount -= calcBase(pendingTrade);
+				pendingTrade = null;
+
 				console.log('Exit sell');
 				break;
-			case 'exit buy':
+			case 'exit buy': // cancel the last buy order
+				if (pendingTrade == null) {
+					console.log('No buy trade to cancel');
+					break;
+				}
 				if (!isUsingHistoricalData) {
 					exchange.cancelTrade({
 						Type: 'Trade',
-						OrderId: currentTradeId,
+						OrderId: pendingTrade.id,
 					});
 				}
+				coinAmount -= pendingTrade.amount;
+				btcAmount += calcBase(pendingTrade);
+				pendingTrade = null;
+
 				console.log('Exit buy');
 				break;
-			case 'hold':
-			default:
-				// hold
+			case 'hold': // keep current holdings
+				console.log('Hold');
+				break;
+			case 'sell closed': // mark last sell order as complete
+				pendingTrade = null;
+				console.log('Sell closed');
+				break;
+			case 'buy closed': // mark last buy order as complete
+				pendingTrade = null;
+				console.log('Buy closed');
+				break;
+			default: // unexpected result
+				console.log(`Couldn't decipher action: ${action}`);
 				break;
 		}
 
-		setTimeout(update, isUsingHistoricalData ? period : 0);
+		logBalance();
+
+		setTimeout(update, !isUsingHistoricalData ? period : 0);
 	};
 
 	// only give strategy the public api functions
--- strategies/moving-average.mjs
+++ strategies/moving-average.mjs
@@ -1,7 +1,6 @@
 export default class MovingAverage {
 	constructor(exchange) {
-		this.currentBalance = 0;
-		this.averageCount = 10;
+		this.averageCount = 300;
 		this.values = [];
 		this.tradePlaced = false;
 		this.tradeType = '';
@@ -18,7 +17,7 @@ export default class MovingAverage {
 		let movingAverage = this.values.reduce((p, c) => p + c, 0) / this.values.length;
 
 		if (!this.tradePlaced) {
-			if (this.currentBalance > 0 && currentValue > movingAverage && currentValue < previousValue) {
+			if (currentValue > movingAverage && currentValue < previousValue) {
 				this.tradePlaced = true;
 				this.tradeType = 'sell';
 				return 'sell';
@@ -32,12 +31,20 @@ export default class MovingAverage {
 				// market has moved down without us, too cheap to sell
 				this.tradePlaced = false;
 				return 'exit sell';
+			} else {
+				// sell succeeded
+				this.tradePlaced = false;
+				return 'sell closed';
 			}
 		} else if (this.tradeType == 'buy') {
 			if (currentValue > movingAverage) {
 				// market has moved up without us, too expensive to buy
 				this.tradePlaced = false;
 				return 'exit buy';
+			} else {
+				// buy succeeded
+				this.tradePlaced = false;
+				return 'buy closed';
 			}
 		}
 
